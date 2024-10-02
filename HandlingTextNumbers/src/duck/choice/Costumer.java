0d588d6be0e0ae137398d29c34dd2ac8378a26f6
Ana Lydia Moreira <analydiamoreira965@gmail.com>
2024-10-02 18:53:53 -0300

    Add Array and Switch

---
+++ HandlingTextNumbers/src/duck/choice/Costumer.java
@@ -0,0 +1,6 @@
+package duck.choice;
+
+public class Costumer {
+    String name;
+    String size;
+}
--- HandlingTextNumbers/src/duck/choice/ShopApp.java
+++ HandlingTextNumbers/src/duck/choice/ShopApp.java
@@ -3,15 +3,20 @@ package duck.choice;
 public class ShopApp {
     public static void main(String[] args) {
         double tax = 0.2;
+        int measurement = 3;
+        Costumer costumer1 = new Costumer();
         Clothing item1 = new Clothing();
         Clothing item2 = new Clothing();
 
+        Clothing[] items = {item1, item2};
+
+        costumer1.name = "Pinky";
+        costumer1.size = "S";
         item1.description = "Blue Jacket";
         item1.price = 20.9;
-        item2.description="Orange T-Shirt";
+        item2.description = "Orange T-Shirt";
         item2.price = 10.5;
         item2.size = "S";
-
         String item1Information = String.format("Item 1: Description: %s, Price: %s, Size: %s",
                 item1.description,
                 item1.price,
@@ -25,8 +30,20 @@ public class ShopApp {
                 item2.size
         );
         System.out.println(item2Information);
-
         double total = (item1.price + item2.price * 2) * (1 + tax);
         System.out.println("Total: " + total);
+
+        switch (measurement) {
+            case 1: case 2: case 3:
+                costumer1.size = "S";
+                break;
+            case 4: case 5: case 6:
+                costumer1.size = "M";
+                break;
+            case 7: case 8: case 9:
+                costumer1.size = "L";
+                break;
+            default:
+        }
     }
 }
